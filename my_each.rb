8602c3bbb3248deae44b344bdf3897e53ef178ac
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 18:36:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,20 @@
 collection = [1, 2, 3, 4]
 
 def my_each(collection)
+  if block_given?
+    i = 0
 
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+my_each(collection) do |i|
+  puts i
 end
